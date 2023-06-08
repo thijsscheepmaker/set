437b39e174c42a0ff2857410b0dbd34225f1e898
thijsscheepmaker <t.scheepmaker1@students.uu.nl>
2023-06-08 11:43:07 +0200

    Update DrawStack.py

--- DrawStack.py
+++ DrawStack.py
@@ -1,5 +1,18 @@
 class DrawStack:
     
-    def __init__(self):
-        
-        return
+    import random as r
+
+    def __init__(self, kleur = 0, vorm = 0, aantal = 0):
+        stack = []
+        self.k = kleur
+        self.v = vorm
+        self.a = aantal
+        for i in range(0,80):
+            stack.append()
+
+
+    def __str__(self):
+        return [self.a, self.v, self.a]
+    
+    def createstack(self):
+        for
